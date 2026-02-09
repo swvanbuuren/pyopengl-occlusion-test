8eaf86c0cb16edb8a9cd9d306768bce3081d1cd9
Sietze van Buuren <s.van.buuren@gmail.com>
2026-02-09 15:28:07 +0100

    Use OpenGL occlusion queries (GL_ANY_SAMPLES_PASSED)
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- src/pyopengl_occlusion_test/occlusion_test.py
+++ src/pyopengl_occlusion_test/occlusion_test.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 from OpenGL import GL, GLU
+
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
 from PySide6.QtOpenGLWidgets import QOpenGLWidget
 from PySide6.QtCore import QTimer
@@ -10,6 +11,7 @@ from PySide6.QtCore import QTimer
 class OcclusionGLWidget(QOpenGLWidget):
     def __init__(self):
         super().__init__()
+
         self.plane_y = None
         self.test_points = []
         self.calculated_occlusion = []
@@ -34,51 +36,67 @@ class OcclusionGLWidget(QOpenGLWidget):
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
         if self.plane_y is None:
             return
-        self._setup_matrices()
-        self._draw_plane()
+
+        self._setup_camera()
+        self._draw_plane(alpha=0.35)
         self._draw_points()
 
     # ---------------- Camera ----------------
 
-    def _setup_matrices(self):
+    def _setup_camera(self):
         w, h = self.width(), self.height()
         GL.glMatrixMode(GL.GL_PROJECTION)
         GL.glLoadIdentity()
         GLU.gluPerspective(45.0, w / max(1, h), 0.1, 100.0)
 
         az, el = np.radians(self.azimuth), np.radians(self.elevation)
-        self.camera_pos = np.array([
-            self.distance * np.cos(el) * np.sin(az),
-            self.distance * np.sin(el),
-            self.distance * np.cos(el) * np.cos(az)
-        ])
+        cam_x = self.distance * np.cos(el) * np.sin(az)
+        cam_y = self.distance * np.sin(el)
+        cam_z = self.distance * np.cos(el) * np.cos(az)
+        self.camera_pos = np.array([cam_x, cam_y, cam_z])
 
         GL.glMatrixMode(GL.GL_MODELVIEW)
         GL.glLoadIdentity()
-        GLU.gluLookAt(*self.camera_pos, *self.target, 0, 1, 0)
+        GLU.gluLookAt(cam_x, cam_y, cam_z, *self.target, 0, 1, 0)
 
     # ---------------- Scene ----------------
 
     def _draw_plane(self, alpha=0.25):
-        y, size = self.plane_y, 1000.0
-        steps = 20
-        step_size = size / steps
+        size = 1000.0
+        y = self.plane_y
 
-        # Translucent fill
+        # Depth-only pass
+        GL.glDepthMask(True)
+        GL.glColorMask(False, False, False, False)
+        GL.glBegin(GL.GL_TRIANGLES)
+        GL.glVertex3f(-size, y, -size)
+        GL.glVertex3f(size, y, -size)
+        GL.glVertex3f(size, y, size)
+        GL.glVertex3f(-size, y, -size)
+        GL.glVertex3f(size, y, size)
+        GL.glVertex3f(-size, y, size)
+        GL.glEnd()
+
+        # Fill pass
+        GL.glColorMask(True, True, True, True)
+        GL.glDepthMask(False)
         GL.glEnable(GL.GL_BLEND)
         GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
         GL.glColor4f(0.2, 0.6, 0.9, alpha)
         GL.glBegin(GL.GL_QUADS)
         GL.glVertex3f(-size, y, -size)
-        GL.glVertex3f( size, y, -size)
-        GL.glVertex3f( size, y,  size)
-        GL.glVertex3f(-size, y,  size)
+        GL.glVertex3f(size, y, -size)
+        GL.glVertex3f(size, y, size)
+        GL.glVertex3f(-size, y, size)
         GL.glEnd()
         GL.glDisable(GL.GL_BLEND)
+        GL.glDepthMask(True)
 
         # Wireframe
         GL.glColor3f(0.8, 0.8, 0.8)
         GL.glLineWidth(1.0)
+        steps = 20
+        step_size = size / steps
         GL.glBegin(GL.GL_LINES)
         for i in range(-steps, steps + 1):
             x = i * step_size
@@ -97,27 +115,29 @@ class OcclusionGLWidget(QOpenGLWidget):
             GL.glVertex3f(*p)
         GL.glEnd()
 
-    # ---------------- Occlusion ----------------
+    # ---------------- Occlusion logic ----------------
 
     def run_occlusion_test(self):
+        """Use OpenGL occlusion queries to check each point."""
         self.makeCurrent()
         self.calculated_occlusion.clear()
-        modelview = GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX)
-        projection = GL.glGetDoublev(GL.GL_PROJECTION_MATRIX)
-        viewport = GL.glGetIntegerv(GL.GL_VIEWPORT)
 
+        # Prepare camera & depth buffer
+        self._setup_camera()
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+        self._draw_plane(alpha=0.35)
+
+        # For each point, test if any pixel passes depth test
         for p in self.test_points:
-            winx, winy, winz = GLU.gluProject(*p, modelview, projection, viewport)
-            x, y, w, h = viewport
-            px, py = int(winx), int(winy)
-
-            if not (x <= px < x + w and y <= py < y + h):
-                self.calculated_occlusion.append(True)
-                continue
-
-            GL.glReadBuffer(GL.GL_COLOR_ATTACHMENT0)
-            depth = GL.glReadPixels(px, py, 1, 1, GL.GL_DEPTH_COMPONENT, GL.GL_FLOAT)
-            self.calculated_occlusion.append(depth[0][0] < winz - 1e-6)
+            query = GL.glGenQueries(1)[0]
+            GL.glBeginQuery(GL.GL_ANY_SAMPLES_PASSED, query)
+            GL.glBegin(GL.GL_POINTS)
+            GL.glVertex3f(*p)
+            GL.glEnd()
+            GL.glEndQuery(GL.GL_ANY_SAMPLES_PASSED)
+            result = GL.glGetQueryObjectuiv(query, GL.GL_QUERY_RESULT)
+            self.calculated_occlusion.append(not bool(result))
+            GL.glDeleteQueries(1, [query])
 
         self.update()
 
@@ -127,7 +147,7 @@ class OcclusionGLWidget(QOpenGLWidget):
         self.last_mouse_pos = e.pos()
 
     def mouseMoveEvent(self, e):
-        if not self.last_mouse_pos:
+        if self.last_mouse_pos is None:
             return
         dx, dy = e.x() - self.last_mouse_pos.x(), e.y() - self.last_mouse_pos.y()
         self.azimuth += dx * 0.5
@@ -146,7 +166,7 @@ class OcclusionGLWidget(QOpenGLWidget):
         np.random.seed(42)
         self.plane_y = np.random.uniform(-2, 2)
         self.test_points = [np.random.uniform(-5, 5, 3) for _ in range(n)]
-        self.calculated_occlusion = [False] * n  # placeholder
+        self.calculated_occlusion = [False] * n
 
     def showEvent(self, event):
         super().showEvent(event)
@@ -154,13 +174,13 @@ class OcclusionGLWidget(QOpenGLWidget):
             QTimer.singleShot(50, self.run_occlusion_test)
 
 
+# ---------------- Main Window ----------------
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("OpenGL Occlusion Debugger")
-
         self.gl = OcclusionGLWidget()
-        self.gl.generate_test_data()
 
         btn = QPushButton("Run occlusion test")
         btn.clicked.connect(self.gl.run_occlusion_test)
@@ -168,6 +188,8 @@ class MainWindow(QWidget):
         layout = QVBoxLayout(self)
         layout.addWidget(self.gl, 1)
         layout.addWidget(btn)
+
+        self.gl.generate_test_data()
 
 
 def main():
